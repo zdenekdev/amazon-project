26f9f96a26693e21dffa41e806ca125025952a5c
zdenekdev <kucera.contact@gmail.com>
2023-08-23 14:18:03 +0200

    banner (carousel)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon project</title>
         <meta name="description" content="Amazon project" />
@@ -12,6 +13,12 @@ export default function Home() {
 
       {/* Header */}
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* Product feed */}
+      </main>
     </div>
   );
 }
