abc7ac418aa30f8dc1e8d1d27cac6f9b68aea697
zdenekdev <kucera.contact@gmail.com>
2023-08-24 18:21:31 +0200

    product feed completion

--- pages/components/ProductFeed.tsx
+++ pages/components/ProductFeed.tsx
@@ -3,10 +3,10 @@ import Product from "./Product";
 
 function ProductFeed({ products }: { products: Products[] }) {
   return (
-    <div>
-      <h1>products here...</h1>
-      {products.map(
-        ({ id, title, price, description, category, image, rating }) => (
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image, rating }) => (
           <Product
             key={id}
             id={id}
@@ -17,8 +17,41 @@ function ProductFeed({ products }: { products: Products[] }) {
             image={image}
             rating={rating}
           />
-        )
-      )}
+        ))}
+
+      <img className="md:col-span-full" src="https://links.papareact.com/dyz" />
+
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image, rating }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+              rating={rating}
+            />
+          ))}
+      </div>
+
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image, rating }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+            rating={rating}
+          />
+        ))}
     </div>
   );
 }
