a6b80cbf4792cbda82f8c0beecb54b5b75358345
zdenekdev <kucera.contact@gmail.com>
2023-08-24 20:25:29 +0200

    Firebase and NextAuth installed

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBiisDTewLX2uAsEaOd6UQ01YPCQRoDr4",
+  authDomain: "amzn-2-yt-8cb12.firebaseapp.com",
+  projectId: "amzn-2-yt-8cb12",
+  storageBucket: "amzn-2-yt-8cb12.appspot.com",
+  messagingSenderId: "668346526735",
+  appId: "1:668346526735:web:0b5cd79362755b8ed1477a",
+};
