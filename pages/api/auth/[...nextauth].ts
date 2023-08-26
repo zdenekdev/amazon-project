a5ff0655873206259a3b2f8634deb7250d1bbba2
zdenekdev <kucera.contact@gmail.com>
2023-08-26 14:18:13 +0200

    authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
@@ -8,13 +9,17 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
   ],
-  callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin";
-      return token;
-    },
-  },
+  // callbacks: {
+  //   async jwt({ token }) {
+  //     token.userRole = "admin";
+  //     return token;
+  //   },
+  // },
 };
 
 export default NextAuth(authOptions);
---
+++ pages/checkout.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function Checkout() {
+  return (
+    <div className="bg-gray-100">
+      <Header />
+      <h1>checkout</h1>
+    </div>
+  );
+}
+
+export default Checkout;
