043bb51eb09e3312a8aa1e2b9b7dad2fe28aa4db
zdenekdev <kucera.contact@gmail.com>
2023-08-27 14:00:37 +0200

    debugging prop products in ProductFeed

--- pages/components/ProductFeed.tsx
+++ pages/components/ProductFeed.tsx
@@ -5,20 +5,21 @@ import Product from "./Product";
 function ProductFeed({ products }: { products: Products[] }) {
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-      {products
-        .slice(0, 4)
-        .map(({ id, title, price, description, category, image, rating }) => (
-          <Product
-            key={id}
-            id={id}
-            title={title}
-            price={price}
-            description={description}
-            category={category}
-            image={image}
-            rating={rating}
-          />
-        ))}
+      {products &&
+        products
+          .slice(0, 4)
+          .map(({ id, title, price, description, category, image, rating }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+              rating={rating}
+            />
+          ))}
 
       <div className="md:col-span-full">
         <Image
